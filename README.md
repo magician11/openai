769ab87f1a492dc882272183b8a7a6aa988d9ff6
magician11 <support@andrewgolightly.com>
2023-03-06 13:01:54 +0530

    setup basic layout

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # openai-personas
 
 Answering any question as various personas using OpenAI
+
+## Sources
+
+Icon: https://www.svgrepo.com/svg/421090/account-avatar-person
+Typewriter: https://www.npmjs.com/package/typewriter-effect
+Font: https://fonts.google.com/specimen/Public+Sans
--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import '@fontsource/public-sans';
-import * as React from 'react';
+import { useState } from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
-import Button from '@mui/joy/Button';
+import {
+  Button,
+  Typography,
+  Textarea,
+  Grid,
+  CssBaseline,
+  Stack
+} from '@mui/joy';
+import Typewriter from 'typewriter-effect';
 
 const App = () => {
+  const persona = 'Jesus';
+  const [response, setResponse] = useState('');
+
+  console.log('response', response);
   return (
     <CssVarsProvider>
-      <Button variant="solid">Hello World</Button>
+      <CssBaseline />
+      <Grid
+        container
+        spacing={2}
+        sx={{ flexGrow: 1, margin: '0 auto', maxWidth: 1111 }}
+      >
+        <Grid xs={12} sx={{ mb: 11 }}>
+          <Typography level="h1" textAlign="center">
+            Ask {persona} anything
+          </Typography>
+        </Grid>
+        <Grid xs={6}>
+          <Stack spacing={2} alignItems="center">
+            <Textarea minRows={2} sx={{ width: 333 }} />
+            <Button
+              variant="solid"
+              sx={{ width: 222 }}
+              onClick={() => setResponse('yes I do')}
+            >
+              Ask
+            </Button>
+          </Stack>
+        </Grid>
+        <Grid xs={6}>
+          <Typography level="body1">
+            <Typewriter
+              options={{
+                strings: [response],
+                autoStart: true
+              }}
+            />
+          </Typography>
+        </Grid>
+      </Grid>
     </CssVarsProvider>
   );
 };
