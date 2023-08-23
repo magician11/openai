58fb2c55fdebac159abfc2a99bb3ec40397ec211
magician11 <support@andrewgolightly.com>
2023-08-23 15:45:55 +0100

    upgraded openai to v4

--- functions/modules/openai.js
+++ functions/modules/openai.js
@@ -1,23 +1,21 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 import { info, error } from 'firebase-functions/logger';
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-});
-const openai = new OpenAIApi(configuration);
 
 // https://platform.openai.com/docs/api-reference/completions/create
 const createCompletion = async (prompt, max_tokens = 333) => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
   info(`About to complete the prompt "${prompt}"..."`);
   try {
-    const completion = await openai.createCompletion({
+    const completion = await openai.completions.create({
       model: 'text-davinci-003',
       prompt,
       max_tokens
     });
 
     info('completion.data', completion.data);
-    return completion.data.choices[0].text;
+    return completion.choices[0].text;
   } catch (err) {
     error('Error completing prompt', err.message);
     throw new Error(err.message);
@@ -26,14 +24,17 @@ const createCompletion = async (prompt, max_tokens = 333) => {
 
 // https://platform.openai.com/docs/api-reference/chat/create
 const createChatCompletion = async messages => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
   // try {
-  const completion = await openai.createChatCompletion({
+  const completion = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     messages
   });
   info('chat completion.data', completion.data);
 
-  return completion.data.choices[0].message;
+  return completion.choices[0].message;
   // } catch (err) {
   //   error('Error with chat completion', err);
   //   throw new Error(err.message);
@@ -42,19 +43,25 @@ const createChatCompletion = async messages => {
 
 // https://platform.openai.com/docs/api-reference/chat/create
 const createChatCompletionBeta = async messages => {
-  const completion = await openai.createChatCompletion({
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
+  const completion = await openai.chat.completions.create({
     model: 'gpt-4-0613',
     messages
   });
   info('chat completion.data (beta)', completion.data);
 
-  return completion.data.choices[0].message;
+  return completion.choices[0].message;
 };
 
 // https://platform.openai.com/docs/api-reference/images/create
 const createImage = async prompt => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
   info(`Generating an image for "${prompt}"`);
-  const response = await openai.createImage({
+  const response = await openai.images.generate({
     prompt
   });
 
